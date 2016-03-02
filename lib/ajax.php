e10bbc6343a363411b015d51ec88a3c7ef752eb9
unknown <you@example.com>
2016-03-02 23:50:17 +0100

    Revert "első commit"
    This reverts commit 488d4dc9ae3c1f4002dff00942c87ed272032c8b.

--- lib/ajax.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('P3P:CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"');
-session_start();
-$_SESSION['fbpont'] = intval($_POST['pont']);
-?>
--- lib/config.php
+++
@@ -1,6 +0,0 @@
-<?
-$fbconfig['appId']  = '217780471699567';
-$fbconfig['secret'] = 'ee0f8b6eecc27178c1abd9b4e0bd23e0';
-$fbconfig['appUrl'] = 'https://applications.mile.hu/duett/';
-//$fbconfig['appUrl'] = 'http://tv2.hu/szorakozas/jatekok/122438_felismered_a_nagy_duett_enekeseit_es_dalait.html';
-?>
